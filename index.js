aa6919cb08c1a828c7edd93d426a75f9e3ea9b40
momolly1024 <momolly1024@gmail.com>
2020-07-06 13:34:30 +0800

    upload

---
+++ index.js
@@ -0,0 +1,398 @@
+import express from 'express'
+import bodyParser from 'body-parser'
+import cors from 'cors'
+import connectMongo from 'connect-mongo'
+import session from 'express-session'
+import md5 from 'md5'
+import dotenv from 'dotenv'
+import path from 'path'
+import db from './db.js'
+import multer from 'multer'
+import FTPStorage from 'multer-ftp'
+// fs 需要研究一下
+import fs from 'fs'
+dotenv.config()
+
+const MongoStore = connectMongo(session)
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(cors({
+  // 跨域請求的判斷
+  origin (origin, callback) {
+    if (process.env.ALLOW_CORS === 'true') {
+      // 開發環境 ， 允許進入！
+      callback(null, true)
+    } else if (origin.includes('github')) {
+      // 非開發環境，但是是從github過來的，允許進入！
+      callback(null, true)
+    } else {
+      // 不是開發環境也不是從github過來的，拒絕進入！
+      callback(new Error('Not allowed'), false)
+    }
+  },
+  credentials: true
+}))
+
+app.use(session({
+  secret: 'albumsecret',
+  // 將session存進mongoDB
+  store: new MongoStore({
+    // 使用mongoose的資料庫連接
+    mongooseConnection: db.connection,
+    // 設定存入的collection
+    collection: process.env.COLLECTION_SESSION
+  }),
+  // 有效期間
+  cookie: {
+    // 1000毫秒 = 1秒鐘
+    // 1秒*60*30=30分鐘
+    maxAge: 1000 * 60 * 30
+  },
+  // 是否保存未修改的session
+  saveUninitialized: false,
+  // 是否每次重設過期時間
+  rolling: true
+}))
+// ---------------------------------------------------
+
+// 上傳
+let storage
+if (process.env.FTP === 'false') {
+  storage = multer.diskStorage({
+    destination (req, file, cb) {
+      cb(null, 'images/')
+    },
+    filename (req, file, cb) {
+      cb(null, Date.now() + path.extname(file.originalname))
+    }
+  })
+} else {
+  // herohu將上傳檔案放伺服器
+  storage = new FTPStorage({
+    basepath: '/',
+    ftp: {
+      host: process.env.FTP_HOST,
+      secure: false,
+      user: process.env.FTP_USER,
+      password: process.env.FTP_PASSWORD
+    },
+    destination (req, file, options, cb) {
+      cb(null, options.basepath + Date.now() + path.extname(file.originalname))
+    }
+  })
+}
+
+const upload = multer({
+  storage,
+  fileFilter (req, file, cb) {
+    if (!file.mimetype.includes('image')) {
+      cb(new multer.MulterError('LIMIT_FORMAT'), false)
+    } else {
+      cb(null, true)
+    }
+  },
+  limits: {
+    fieldSize: 1024 * 1024
+  }
+})
+
+// ---------------------------------------------------
+
+app.listen(process.env.PORT, () => {
+  console.log('已啟動')
+})
+
+// 註冊帳號
+app.post('/users', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+
+  try {
+    await db.users.create({
+      account: req.body.account,
+      password: md5(req.body.password)
+    })
+    res.status(200)
+    res.send({ success: true, message: '' })
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 登入
+app.post('/login', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+
+  try {
+    const result = await db.users.find(
+      {
+        account: req.body.account,
+        password: md5(req.body.password)
+      }
+    )
+
+    if (result.length > 0) {
+      req.session.user = result[0].account
+      res.status(200)
+      res.send({ success: true, message: '' })
+    } else {
+      res.status(404)
+      res.send({ success: false, message: '帳號密碼錯誤' })
+    }
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 登出
+app.delete('/logout', async (req, res) => {
+  req.session.destroy(error => {
+    if (error) {
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    } else {
+      res.clearCookie()
+      res.status(200)
+      res.send({ success: true, message: '' })
+    }
+  })
+})
+
+// 回傳使用者是否有登入給前端 heartbeat:維持住後端的會話session時間
+// 檢查後端是否有過期 過期就把登入狀態清掉
+// 1.前端的登入狀態寫在vueX，vuex不會自動查詢是否維持登入狀態。故每次網頁打開就跟後端要資料是否有登入，有舊延遲30分 沒有就表示已超過30分就過期
+app.get('/heartbeat', async (req, res) => {
+  let isLogin = false
+  if (req.session.user !== undefined) {
+    isLogin = true
+  }
+  res.status(200)
+  res.send(isLogin)
+})
+
+// 上船功能
+app.post('/file', async (req, res) => {
+  // 沒有登入
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
+  if (!req.headers['content-type'].includes('multipart/form-data')) {
+    // .includes('multipart/form-data')為from表單且有包含檔案
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+  // 上傳
+  // 有一個上傳進來的檔案，欄位是 image
+  // req，進來的東西
+  // res，要出去的東西
+  // err，檔案上傳的錯誤
+  // upload.single(欄位)(req, res, 上傳完畢的 function)
+  upload.single('image')(req, res, async error => {
+    if (error instanceof multer.MulterError) {
+      // 上傳錯誤
+      let message = ''
+      if (error.code === 'LIMIT_FILE_SIZE') {
+        message = '檔案太大'
+      } else {
+        message = '格式不符'
+      }
+      res.status(400)
+      res.send({ success: false, message })
+    } else if (error) {
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    } else {
+      try {
+        let name = ''
+        if (process.env.FTP === 'true') {
+          name = path.basename(req.file.path)
+        } else {
+          name = req.file.filename
+        }
+        const result = await db.files.create({
+          user: req.session.user,
+          description: req.body.description,
+          name
+        })
+        res.status(200)
+        res.send({ success: true, message: '', name, _id: result._id })
+      } catch (error) {
+        if (error.name === 'ValidationError') {
+          // 資料格式錯誤
+          const key = Object.keys(error.errors)[0]
+          const message = error.errors[key].message
+          res.status(400)
+          res.send({ success: false, message })
+        } else {
+          // 伺服器錯誤
+          res.status(500)
+          res.send({ success: false, message: '伺服器錯誤' })
+        }
+      }
+    }
+  })
+})
+
+// 取得圖片
+app.get('/file/:name', async (req, res) => {
+  // 沒有登入
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
+  if (process.env.FTP === 'false') {
+    const path = process.cwd() + '/images/' + req.params.name
+    const exists = fs.existsSync(path)
+    if (exists) {
+      res.status(200)
+      res.sendFile(path)
+    } else {
+      res.status(404)
+      res.send({ success: false, message: '找不到圖片' })
+    }
+  } else {
+    res.redirect('http://' + process.env.FTP_HOST + '/' + process.env.FTP_USER + '/' + req.params.name)
+  }
+})
+
+// 獲取單個使用者的所有圖片
+app.get('/album/:user', async (req, res) => {
+  // 沒有登入
+  if (req.session.user === undefined) {
+    res.status(401)
+    res.send({ success: false, message: '未登入' })
+    return
+  }
+  if (req.session.user !== req.params.user) {
+    res.status(403)
+    res.send({ success: false, message: '無權限' })
+    return
+  }
+  try {
+    const result = await db.files.find({ user: req.params.user })
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    res.status(500)
+    res.send({ success: false, message: '伺服器錯誤' })
+  }
+})
+
+// 更新說明欄位
+app.patch('/file/:id', async (req, res) => {
+  if (!req.headers['content-type'].includes('application/json')) {
+    res.status(400)
+    res.send({ success: false, message: '格式不符' })
+    return
+  }
+  // 沒有登入
+  if (!req.session.user) {
+    res.status(401)
+    res.send({ success: false, message: '無權限' })
+    return
+  }
+
+  try {
+    // 檢查相片擁有者是不是本人
+    let result = await db.files.findById(req.params.id)
+    if (result.user !== req.session.user) {
+      res.status(403)
+      res.send({ success: false, message: '無權限' })
+      return
+    }
+    // findByIdAndUpdate 預設回傳的是更新前的資料
+    // 設定 new true 後會變成回傳新的資料
+    result = await db.files.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    res.status(200)
+    res.send({ success: true, message: '', result })
+  } catch (error) {
+    if (error.name === 'CastError') {
+      // ID 格式不是 MongoDB 的格式
+      res.status(400)
+      res.send({ success: false, message: 'ID 格式錯誤' })
+    } else if (error.name === 'ValidationError') {
+      // 資料格式錯誤
+      const key = Object.keys(error.errors)[0]
+      const message = error.errors[key].message
+      res.status(400)
+      res.send({ success: false, message })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
+
+// 刪除照片
+
+app.delete('/file/:id', async (req, res) => {
+  // 沒有登入
+  if (!req.session.user) {
+    res.status(401)
+    res.send({ success: false, message: '無權限' })
+    return
+  }
+
+  try {
+    // 檢查相片擁有者是不是本人
+    let result = await db.files.findById(req.params.id)
+    if (result.user !== req.session.user) {
+      res.status(403)
+      res.send({ success: false, message: '無權限' })
+      return
+    }
+    // findByIdAndUpdate 預設回傳的是更新前的資料
+    // 設定 new true 後會變成回傳新的資料
+    result = await db.files.findByIdAndDelete(req.params.id)
+    if (result === null) {
+      res.status(404)
+      res.send({ success: false, message: '找不到資料' })
+    } else {
+      res.status(200)
+      res.send({ success: true, message: '', result })
+    }
+  } catch (error) {
+    if (error.name === 'CastError') {
+      res.status(400)
+      res.send({ success: false, message: 'ID 格式錯誤' })
+    } else {
+      // 伺服器錯誤
+      res.status(500)
+      res.send({ success: false, message: '伺服器錯誤' })
+    }
+  }
+})
